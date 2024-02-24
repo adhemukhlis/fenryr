cce5faae4dbc9f32dc3737759170fb9b1ef7f780
adhemukhlis <adhemukhlis@gmail.com>
2024-02-24 22:06:36 +0700

    add install docker

--- .storybook/fenryrTheme.js
+++ .storybook/fenryrTheme.js
@@ -1,7 +1,8 @@
 import { create } from '@storybook/theming';
 
 export default create({
-  brandTitle: 'Fenryr Storybook',
-  brandImage: '../src/assets/Pornhub-logo.png',
+  // brandTitle: 'Fenryr Storybook',
+  brandTitle: `<span style="display:flex;gap:20px;align-items:center;"><img src="../src/assets/fenrir.png" width="60px" height="60px"/><h1 style="font-weight:800;">Fenryr</h1></span>`,
+  // brandImage: '../src/assets/fenrir.png',
   brandTarget: '_self',
 });
